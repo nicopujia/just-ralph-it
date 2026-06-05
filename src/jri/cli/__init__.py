c7a81f1252c61ce8bb61ceb14927f7616a5ffa59
Nicolás Pujia <nicopujia.dev@gmail.com>
2026-06-04 19:09:53 -0700

    docs(package): specify package purposes

--- src/jri/cli/__init__.py
+++ src/jri/cli/__init__.py
@@ -1,4 +1,9 @@
-"""Command-line interface client."""
+"""Command-line interface client.
+
+This package is no more than a thin wrapper
+around the SDK provided by `jri.core`, containing only logic
+related to the user interface in the terminal.
+"""
 
 from .main import main
 
--- src/jri/core/__init__.py
+++ src/jri/core/__init__.py
@@ -1,1 +1,5 @@
-"""Main business logic."""
+"""Main business logic.
+
+This package provides the functionality that powers JRI.
+It has no public interface beyond the public SDK.
+"""
